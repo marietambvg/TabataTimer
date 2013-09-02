69f86437b97c727c38d8f9ce97ad7479e4105db3
Marieta Georgieva <marietambvg@gmail.com>
2013-09-02 17:26:58 +0300

    Add roaming

--- TabataIntervalTimer/Tabata/js/datalayer.js
+++ TabataIntervalTimer/Tabata/js/datalayer.js
@@ -1,34 +1,62 @@
-﻿(function () {
+﻿/// <reference path="../roamingdata.js" />
+(function () {
 
     var tabatas = [
-            new Models.TabataModel("standard triple",12,10,20,10),
-            new Models.TabataModel("standard half",4,10,20,10),
-            new Models.TabataModel("ironman half",4,10,50,10),
+            new Models.TabataModel("standard triple", 12, 10, 20, 10),
+            new Models.TabataModel("standard half", 4, 10, 20, 10),
+            new Models.TabataModel("ironman half", 4, 10, 50, 10),
             new Models.TabataModel("ironman triple", 12, 10, 50, 10),
             new Models.TabataModel("quick test", 2, 3, 3, 3),
-    ]
+    ];
+
+    var updateRoamingStore = function () {
+        RoamingCodeBehind.storeData(tabatas);
+    }
+
+
+    var getUpdatedTabatas = function () {
+        tabatas = [];
+        var roamingData = RoamingCodeBehind.getRoamingData();
+        var currentModel;
+        for (var i = 0; i < roamingData.size; i++) {
+            var name = roamingData["workout" + i].name;
+            var intervals = roamingData["workout" + i].intervals;
+            var rest = roamingData["workout" + i].rest;
+            var work = roamingData["workout" + i].work;
+            var prepare = roamingData["workout" + i].prepare;
+            currentModel = new Models.TabataModel(name, intervals, rest, work, prepare);
+            tabatas.push(currentModel);
+        }
+
+        return tabatas;
+    }
+
 
     var getTabatas = function () {
         return tabatas;
     }
 
+
+
     var getBasicTabata = function () {
-        return  new Models.TabataModel("standard",8,10,20,10)
+        return new Models.TabataModel("standard", 8, 10, 20, 10)
     }
 
-    var getTabata = function (name,intervals,rest,work,prepare) {
+    var getTabata = function (name, intervals, rest, work, prepare) {
         return new Models.TabataModel(name, intervals, rest, work, prepare)
     }
 
     var addTabata = function (tabataModel) {
-       // if(tabata
+        // if(tabata
         tabatas.push(tabataModel);
+        updateRoamingStore();
     }
 
     WinJS.Namespace.define("Data", {
-        getTabatas: getTabatas,
+        getTabatas: getUpdatedTabatas,
+        getUpdatedTabatas: getUpdatedTabatas,
         addTabata: addTabata,
         getBasicTabata: getBasicTabata,
-        getTabata:getTabata
+        getTabata: getTabata
     });
 })()
---
+++ TabataIntervalTimer/Tabata/js/roamingdata.js
@@ -0,0 +1,70 @@
+﻿(function () {
+
+    var applicationData = Windows.Storage.ApplicationData.current;
+
+    function initialize() {
+        applicationData.addEventListener("datachanged", datachangeHandler);
+    }
+
+    function dataChangeHandler(eventArgs) {
+        // TODO: Refresh your data
+    }
+    var roamingSettings = applicationData.roamingSettings;
+    var roamingFolder = applicationData.roamingFolder;
+
+    // Simple setting
+
+    var settingName = "exampleSetting";
+    var settingValue = "Hello World";
+
+    roamingSettings.values[settingName] = settingValue;
+    //or
+
+
+
+    function settingsWriteSetting() {
+        roamingSettings.values[settingName] = settingValue;
+    }
+
+    function settingsDeleteSetting() {
+        roamingSettings.values.remove(settingName);
+    }
+
+    // store the data
+    var storeData = function (data) {
+
+        var container = roamingSettings.createContainer("workoutsContainer",
+                                                            Windows.Storage.ApplicationDataCreateDisposition.Always);
+
+        for (var i = 0; i < data.length; i++) {
+            if (roamingSettings.containers.hasKey("workoutsContainer")) {
+
+                var woDetails = new Windows.Storage.ApplicationDataCompositeValue();
+                woDetails["name"] = data[i].name;
+                woDetails["intervals"] = data[i].intervals;
+                woDetails["rest"] = data[i].rest;
+                woDetails["work"] = data[i].work;
+                woDetails["prepare"] = data[i].prepare;
+                roamingSettings.containers.lookup("workoutsContainer").values["workout"+i] = woDetails;
+            }
+        }
+    }
+
+    var getRoamingData = function () {
+        var hasContainer = roamingSettings.containers.hasKey("workoutsContainer");
+        if (hasContainer) {
+            var roamingData=roamingSettings.containers.lookup("workoutsContainer").values;
+            return roamingData;
+        }
+    }
+    WinJS.Utilities.markSupportedForProcessing(storeData);
+    WinJS.Utilities.markSupportedForProcessing(getRoamingData);
+
+    WinJS.Namespace.define("RoamingCodeBehind", {
+        storeData: storeData,
+        getRoamingData:getRoamingData
+    });
+
+})();
+
+
