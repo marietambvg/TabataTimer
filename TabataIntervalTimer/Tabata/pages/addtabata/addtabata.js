00cba7824f06a9df128d78fdf1073991c6110daa
Marieta Georgieva <marietambvg@gmail.com>
2013-09-03 15:27:35 +0300

    Added session state

--- TabataIntervalTimer/Tabata/pages/addtabata/addtabata.js
+++ TabataIntervalTimer/Tabata/pages/addtabata/addtabata.js
@@ -2,21 +2,34 @@
 // http://go.microsoft.com/fwlink/?LinkId=232511
 (function () {
     "use strict";
-
+    var app = WinJS.Application;
     WinJS.UI.Pages.define("/pages/addtabata/addtabata.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
+        processed: function (element, options) {
+            
+            if (app.sessionState.person) {
+                document.getElementById("tabata-name-input").value = app.sessionState.person.name;
+                document.getElementById("wo-intervals").value = app.sessionState.person.intervals;
+                document.getElementById("wo-rest").value = app.sessionState.person.rest;
+                document.getElementById("wo-work").value = app.sessionState.person.work;
+                document.getElementById("wo-prepare").value = app.sessionState.person.prepare;
+            }
+        },
+
         ready: function (element, options) {
-            // TODO: Initialize the page here.
+            
         },
 
-        unload: function () {
-            // TODO: Respond to navigations away from this page.
-        },
-
-        updateLayout: function (element, viewState, lastViewState) {
-            /// <param name="element" domElement="true" />
-            // TODO: Respond to changes in viewState.
+        checkpoint: function () {
+            
+            var tabata = {};
+            tabata.name = document.getElementById("tabata-name-input").value;
+            tabata.intervals = document.getElementById("wo-intervals").value;
+            tabata.rest = document.getElementById("wo-rest").value;
+            tabata.work = document.getElementById("wo-work").value;
+            tabata.prepare = document.getElementById("wo-prepare").value;
+            app.sessionState.tabata = tabata;
         }
     });
 })();
